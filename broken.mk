16a02e6a627ba591f81544460ddd5731b050e399
David Vandenburgh <Seraph08@users.noreply.github.com>
2016-04-18 05:33:32 -0400

    Added files via upload

--- broken.mk
+++ broken.mk
@@ -1,11 +1,10 @@
-#
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,5 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/broken_oneplus2.mk
+$(call inherit-product, device/oneplus/oneplus2/full_oneplus2.mk)
+
+# Inherit some common Broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
