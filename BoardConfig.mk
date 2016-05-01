2d43790f93e288c6fd946a02f3ff2979b877dbf1
David Vandenburgh <Seraph08@users.noreply.github.com>
2016-05-01 03:28:55 -0400

    dttw or GTFO

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -213,6 +213,7 @@ BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 BOARD_USES_QC_TIME_SERVICES := true
 
 # CM Hardware
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
