5d700436e9d1835f2a91182c2aad9bbaea8183eb
David Vandenburgh <Seraph08@users.noreply.github.com>
2016-05-06 11:15:46 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -285,4 +285,4 @@ PRODUCT_PACKAGES += \
     hostapd.deny
 
 # Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
+#$(call inherit-product, device/oppo/common/common.mk)
